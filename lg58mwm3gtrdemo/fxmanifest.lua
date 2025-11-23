3f0c1964530ee4d5926a33a86291898dd778dbb2
leela-caitlyn <elena.plataunaite@gmail.com>
2025-11-23 09:01:24 +0200

    initial

---
+++ lg58mwm3gtrdemo/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+game "gta5"
+
+files {
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg58mwm3gtrdemo_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg58mwm3gtrdemo_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg58mwm3gtrdemo_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg58mwm3gtrdemo"
